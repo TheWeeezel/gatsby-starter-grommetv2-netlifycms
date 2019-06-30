7092b06f477b146b8f02ea57bb8aca94d5a03f9a
TheWeeezel <d.richner@vedea.ch>
2019-06-30 17:55:58 +0200

    feat: removed unnecessary files

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,5 +1,5 @@
-import React from "react"
+// import React from "react"
 
-export default () => {
-  return <div>Hello Page</div>
-}
+// export default () => {
+//   return <div>Hello Page</div>
+// }
